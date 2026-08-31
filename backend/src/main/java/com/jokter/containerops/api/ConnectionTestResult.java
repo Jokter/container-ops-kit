0d307fa3d5ef44c69f031a6fd964d10646a05139
Chef <57486085+Jokter@users.noreply.github.com>
2026-08-31 15:28:39 +0800

    refactor: remove legacy layered packages

--- backend/src/main/java/com/jokter/containerops/api/ConnectionTestResult.java
+++
@@ -1,5 +0,0 @@
-package com.jokter.containerops.api;
-
-import com.jokter.containerops.domain.ConnectionStatus;
-
-public record ConnectionTestResult(ConnectionStatus status, Long latencyMs, String error) {}
