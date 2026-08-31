1d0c6bf3195dd5838af70e522165ce0b7f98bd7c
Chef <57486085+Jokter@users.noreply.github.com>
2026-08-31 09:29:02 +0800

    feat: add resource center backend foundation

---
+++ backend/src/main/java/com/jokter/containerops/ContainerOpsKitApplication.java
@@ -0,0 +1,11 @@
+package com.jokter.containerops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerOpsKitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerOpsKitApplication.class, args);
+    }
+}
