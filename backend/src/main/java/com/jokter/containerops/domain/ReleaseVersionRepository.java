8d746559507f73152dea97eec31f84853c214b66
Chef <57486085+Jokter@users.noreply.github.com>
2026-08-31 09:30:15 +0800

    feat: add environment API and SSH testing

---
+++ backend/src/main/java/com/jokter/containerops/domain/ReleaseVersionRepository.java
@@ -0,0 +1,5 @@
+package com.jokter.containerops.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReleaseVersionRepository extends JpaRepository<ReleaseVersion, Long> {}
