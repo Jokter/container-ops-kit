a54980e0c90e7c9964ba54cda759ff3628475ca9
Chef <57486085+Jokter@users.noreply.github.com>
2026-08-31 15:32:31 +0800

    refactor: remove legacy layered packages

--- backend/src/main/java/com/jokter/containerops/domain/EnvironmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jokter.containerops.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EnvironmentRepository extends JpaRepository<Environment, Long> {
-    List<Environment> findAllByOrderByUpdatedAtDesc();
-}
