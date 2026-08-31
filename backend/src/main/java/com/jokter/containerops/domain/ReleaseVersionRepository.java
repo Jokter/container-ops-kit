f8a3a6da9a8b2bfb1da8a866f0d6b28668251df3
Chef <57486085+Jokter@users.noreply.github.com>
2026-08-31 15:32:43 +0800

    refactor: remove legacy layered packages

--- backend/src/main/java/com/jokter/containerops/domain/ReleaseVersionRepository.java
+++
@@ -1,5 +0,0 @@
-package com.jokter.containerops.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReleaseVersionRepository extends JpaRepository<ReleaseVersion, Long> {}
